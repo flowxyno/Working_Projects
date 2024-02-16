3e256bffaa35b21f45bb34baee7710548e2cf73f
flowxyo <tracy.l.harvey6@gmail.com>
2024-02-16 13:32:08 -0800

    Added sandbox:false and contextIsolation:false to main.js to get the fs function to work

--- IS480 - Senior Internship/Code/Template_and_Test/Initial_test/main.js
+++ IS480 - Senior Internship/Code/Template_and_Test/Initial_test/main.js
@@ -5,7 +5,9 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            sandbox: false,
+            nodeIntegration: true,
+            contextIsolation: false
         }
     });
 
